39da4bf9d0ee4b2bfc59f12ee49baf22f0e99817
Mattias Didriksson <mattias.didriksson@gmail.com>
2017-12-18 23:23:55 +0100

    Im not there yet but I'm close :)

--- src/adventofcode2017/day14.clj
+++ src/adventofcode2017/day14.clj
@@ -22,7 +22,62 @@
   (map #(day10/getHash %) (getRows input size))
 )
 
+(defn getDefragItem [char]
+  (if (= char \1)
+    {:status :used  :group nil}
+    {:status :empty :group nil}
+  )
+)
+
+(defn getDefragRow [rowString] 
+  (into [] (map getDefragItem rowString))
+)
+
+(defn create2DArray [input size]
+  (into [] (map getDefragRow (map rowToHex (getBinaryString input size))))  
+)
+
+(defn checkAndUpdateNode [data currentPos comparePos]
+  (let [current (get-in data currentPos) compare (get-in data comparePos)]
+    (if (= (get current :status) :empty)
+      data
+      (if (= (get compare :status) :empty)
+        data
+        (assoc-in data currentPos (assoc current :group (get compare :group)))))))
+
+(defn checkAbove [data rownumber]
+  (let [indexes (for [x (range (count (first data)))]
+    [rownumber x])]
+    (loop [state data indexList indexes]
+      (if (empty? indexList)
+        state
+        (recur (checkAndUpdateNode state (first indexList) [(dec (first (first indexList))) 
+          (second (first indexList)) ]) (rest indexList))))))
+
+(defn checkThyNeighbours [data]
+    (let [indexes (for [x (range (count data)) y (range (count (first data)))]
+      [x y])]
+      (loop [state data indexList indexes]
+        (if (empty? indexList)
+          state
+          (recur (checkAndUpdateNode state (first (first indexList)) (second (first indexList))) (rest indexList))
+        )
+      )
+    )
+)
+
+(defn getNumberOfNeightbourgroups [input]
+  (->
+    (checkThyNeighbours input)
+    (flatten)
+    (distinct)
+    (count)
+    (dec)
+  )
+)
+
 (defn solvepuzzle []
   (println "Answer puzzle 1: "
     (count (re-seq #"1" (str/join (map rowToHex (getBinaryString "hwlqcszp" 128)))))
+  )
 )
--- test/adventofcode2017/day14_test.clj
+++ test/adventofcode2017/day14_test.clj
@@ -2,3 +2,78 @@
 (:require [clojure.test :refer :all]
 [adventofcode2017.day14 :refer :all]))
 
+(testing "testing settings regions all the same"
+    (is (= (checkThyNeighbours [
+        [{:status :empty, :group nil} {:status :used, :group "MITTEN"} {:status :used, :group "HÖGER"}]
+        [{:status :used, :group nil} {:status :used, :group nil} {:status :used, :group nil}]]
+        [{:status :used, :group nil} {:status :used, :group nil} {:status :used, :group nil}]
+        ]) 
+        [
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+        ]        
+        )
+    )    
+)
+
+
+(testing "testing settings regions all empty"
+    (is (= (checkThyNeighbours [
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        ]) 
+        [
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        [{:status :empty, :group nil} {:status :empty, :group nil} {:status :empty, :group nil}]
+        ]        
+        )
+    )    
+)
+
+(testing "testing settings regions isolated"
+    (is (= (checkThyNeighbours [
+            [{:status :used, :group nil} {:status :empty, :group nil} {:status :used, :group nil}]
+            [{:status :empty, :group nil} {:status :used, :group nil} {:status :empty, :group nil}]
+            [{:status :used, :group nil} {:status :empty, :group nil} {:status :used, :group nil}]
+        ]) 
+        [
+            [{:status :used, :group "00"} {:status :empty, :group nil} {:status :used, :group "02"}]
+            [{:status :empty, :group nil} {:status :used, :group "11"} {:status :empty, :group nil}]
+            [{:status :used, :group "20"} {:status :empty, :group nil} {:status :used, :group "22"}]
+        ]        
+        )
+    )    
+)
+
+(testing "testing crocked T shape"
+    (is (= (checkThyNeighbours [
+            [{:status :used, :group nil} {:status :used, :group nil} {:status :used, :group nil}]
+            [{:status :empty, :group nil} {:status :used, :group nil} {:status :empty, :group nil}]
+            [{:status :used, :group nil} {:status :empty, :group nil} {:status :used, :group nil}]
+        ]) 
+        [
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+            [{:status :empty, :group nil} {:status :used, :group "00"} {:status :empty, :group nil}]
+            [{:status :used, :group "20"} {:status :empty, :group nil} {:status :used, :group "22"}]
+        ]        
+        )
+    )    
+)
+
+(testing "testing full T"
+    (is (= (checkThyNeighbours [
+            [{:status :used, :group nil} {:status :used, :group nil} {:status :used, :group nil}]
+            [{:status :empty, :group nil} {:status :used, :group nil} {:status :empty, :group nil}]
+            [{:status :used, :group nil} {:status :used, :group nil} {:status :used, :group nil}]
+        ]) 
+        [
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+            [{:status :empty, :group nil} {:status :used, :group "00"} {:status :empty, :group nil}]
+            [{:status :used, :group "00"} {:status :used, :group "00"} {:status :used, :group "00"}]
+        ]        
+        )
+    )    
+)
